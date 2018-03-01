fb6f403a6c6cc91e07e61e54fdf4c840a4b9e30d
Luke <lukecochrane2@gmail.com>
2018-03-01 10:09:42 -0500

    Removing category id from router

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Posts from '@/components/Posts'
 import Post from '@/components/Post'
-import Category from '@/components/Category'
 
 Vue.use(Router)
 
@@ -12,14 +11,7 @@ export default new Router({
     {
       path: '/',
       name: 'Posts',
-      component: Posts,
-      children: [
-        {
-          path: ':id',
-          name: 'category',
-          component: Category
-        }
-      ]
+      component: Posts
     },
     {
       path: '/posts/:postSlug',
